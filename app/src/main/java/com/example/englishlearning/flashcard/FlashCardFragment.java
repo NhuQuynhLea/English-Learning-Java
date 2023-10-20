ace58c002b918ce1a2be61e3a0c4422232511a8e
Lê Như Quỳnh <110320254+NhuQuynhLea@users.noreply.github.com>
2023-10-20 16:03:10 +0700

    Fix xml flashcard fragment

--- app/src/main/java/com/example/englishlearning/flashcard/FlashCardFragment.java
+++ app/src/main/java/com/example/englishlearning/flashcard/FlashCardFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
@@ -14,9 +15,13 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.englishlearning.R;
@@ -38,6 +43,9 @@ public class FlashCardFragment extends Fragment implements OpenDialogListener  {
     private ArrayList<Term> flashCardList;
     private Module module;
     private ProgressBar progressBar;
+    private ImageView imgBackground;
+    private TextView title;
+    private TextView num;
 
 
 
@@ -59,8 +67,10 @@ public class FlashCardFragment extends Fragment implements OpenDialogListener  {
         flashCardViewModel.getAllTermsById(module.getId()).observe(getViewLifecycleOwner(), new Observer<List<Term>>() {
             @Override
             public void onChanged(List<Term> terms) {
-                flashCardList = (ArrayList<Term>) terms;
+                flashCardList.addAll(terms);
                 adapter.setTerms(terms);
+                if(!flashCardList.isEmpty())
+                    num.setText(flashCardList.size() + " Terms");
 
             }
         });
@@ -71,18 +81,41 @@ public class FlashCardFragment extends Fragment implements OpenDialogListener  {
                 Navigation.findNavController(view).navigate(R.id.action_flashCardFragment_to_mainFragment);
             }
         });
-        binding.fabFlashCard.setOnClickListener(new View.OnClickListener() {
 
+
+        binding.imgMenu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                openDialog(module.getId(), null);
+                PopupMenu popupMenu = new PopupMenu(getContext(), binding.imgMenu);
+                popupMenu.inflate(R.menu.popup_menu_flashcard);
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        if(menuItem.getItemId() == R.id.deleteAll){
+                            onDeleteAll();
+                        }
+                        else if(menuItem.getItemId() == R.id.addCard){
+                            addCard();
+                        }
+                        return false;
+                    }
+                });
+                popupMenu.show();
             }
+
         });
 
 
 
         return binding.getRoot();
+    }
+
+    private void onDeleteAll() {
+        flashCardViewModel.deleteAllTerms(module.getId());
+    }
+
+    private void addCard() {
+        openDialog(module.getId(), null);
     }
 
     @Override
@@ -91,6 +124,9 @@ public class FlashCardFragment extends Fragment implements OpenDialogListener  {
         initComponents();
         ItemTouchHelper itemTouchHelper = new ItemTouchHelper(new SwiftToDeleteItem(adapter,flashCardViewModel,view));
         itemTouchHelper.attachToRecyclerView(recyclerView);
+        title.setText(module.getTitle());
+        num.setText("");
+
     }
 
     private void initComponents() {
@@ -100,6 +136,9 @@ public class FlashCardFragment extends Fragment implements OpenDialogListener  {
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerView.setAdapter(adapter);
         progressBar = binding.progressBar;
+        title = binding.txtTitle;
+        num = binding.txtNum;
+        imgBackground = binding.imageView;
     }
 
     public void openDialog(int moduleId, Term term){
